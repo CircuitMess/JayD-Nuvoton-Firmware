62956df370a42666afc69911e19e58435907f615
Domagoj <domagoj.zoraja@fer.hr>
2021-02-15 16:55:36 +0100

    added lastNode & enable/disable int option

--- Include/list.h
+++ Include/list.h
@@ -13,3 +13,6 @@ void addNewNode(uint8_t devID, uint8_t val);
 
 extern struct Node *rootNode;
 extern uint8_t nodeNum;
+extern struct Node *sliderNode[3];
+extern struct Node *lastNode;
+
--- listHandler.c
+++ listHandler.c
@@ -1,63 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "N76E616.h"
+#include "SFR_Macro.h"
 #include "list.h"
+#include "bool.h"
 
 struct Node *rootNode = NULL;
 uint8_t nodeNum = 0;
+struct Node *sliderNode[3] = {NULL};
+struct Node *lastNode = NULL;
 
-uint8_t getNodeNumber(){
-
-	struct Node *temp = rootNode;
-	uint8_t nodeNumber = 0;
-	
-	if(!rootNode)
-		return 0;
-
-	
-	while(temp != NULL){
-		
-		nodeNumber++;
-		
-		temp = temp->nextNode;
-	}
-	
-	return nodeNumber;
-	
-}
 
 void addNewNode(uint8_t devID, uint8_t val){
 		
-		struct Node *newNode;
+		clr_EA;
 	
-		if(nodeNum > 5)
+		if(nodeNum > 10){
+			set_EA;
 			return;
-	
-		newNode = (struct Node*)malloc(sizeof(struct Node));
-
-		newNode->deviceID = devID;
-		newNode->value = val;
-		newNode->nextNode = NULL;
-			
-		if(!rootNode){
-			rootNode = newNode;
 		}
-		
+
+		if((sliderNode[0] != NULL) && (devID == 0x20)){
+			sliderNode[0]->value = val;
+		}
+		else if((sliderNode[1] != NULL) && (devID == 0x21)){
+			sliderNode[1]->value = val;
+		}
+		else if((sliderNode[2] != NULL) && (devID == 0x22)){
+			sliderNode[2]->value = val;
+		}
 		else{
-			struct Node *currentNode = rootNode;
 			
-			while(currentNode->nextNode != NULL){
-				currentNode = currentNode->nextNode;
-			}
-			currentNode->nextNode = newNode;
-				
-		}
+			struct Node *newNode;
+			
+			newNode = (struct Node*)malloc(sizeof(struct Node));
+			
+			newNode->deviceID = devID;
+			newNode->value = val;
+			newNode->nextNode = NULL;
+			
+			nodeNum++;
 		
-		//nodeNum = getNodeNumber();
-		nodeNum++;	
-}		
-
-/*	Search List
-int searchList(uint8_t searchID){
+			if(!rootNode){
+				rootNode = lastNode = newNode;
+			}
+			
+			else{
+			
+				lastNode->nextNode = newNode;
+				lastNode = newNode;
+			}
+		
+			
+			if(sliderNode[0] == NULL && devID == 0x20){
+				sliderNode[0] = newNode;
+			}
+			else if(sliderNode[1] == NULL && devID == 0x21){
+				sliderNode[1] = newNode;
+			}
+			else if(sliderNode[2] == NULL && devID == 0x22){
+				sliderNode[2] = newNode;
+			}
+		
+	}
+	
+	set_EA;
+}
 
 	struct Node *temp = rootNode;
 	struct Node *prev = rootNode;
