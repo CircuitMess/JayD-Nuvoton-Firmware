5dd90237bc876d9eac1031daeaa460723bfce6b5
Domagoj <domagoj.zoraja@fer.hr>
2021-03-24 16:41:47 +0100

    add comments

--- inc/i2c.h
+++ inc/i2c.h
@@ -2,11 +2,12 @@
 #define P23_OpenDrain_Mode P2M1|= SET_BIT3;P2M2|= SET_BIT3
 #define P24_OpenDrain_Mode P2M1|= SET_BIT4;P2M2|= SET_BIT4
 
-#define SLAVE_ADDR 0x43//  67_(10)
+#define SLAVE_ADDR 0x43			//  slave address (ESP->master, N76->slave)
 
-#define STATUS_UPDATE 0x12//  18_(10)
-#define EVENT_HANDLER 0x13//  19_(10)
-#define ID_VERIFICATION 0//  returns slave address 0x43
-#define RESET_COMMAND 1//  resets uC
+#define STATUS_UPDATE 0x12		//  update number of events
+#define EVENT_HANDLER 0x13		//  handle list events
+#define INITIAL_POT_VALUE 0x14	//	initialize pot values
+#define ID_VERIFICATION 0		//  return slave address 0x43
+#define RESET_COMMAND 1			//  reset uC
 
 void initI2C();
